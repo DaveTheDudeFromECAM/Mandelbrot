f305557d928e9428ac2f0e06fa68bd57fd63a4ad
Dawid Krasowski <72083097+DaveTheDudeFromECAM@users.noreply.github.com>
2023-01-07 14:26:03 +0100

    Mandelbrot can generate files with http requests

--- mandelbrot-backend/web-service-gin/main.go
+++ mandelbrot-backend/web-service-gin/main.go
@@ -9,11 +9,16 @@ import (
 	"github.com/faiface/pixel/pixelgl"
 
 	"fmt"
-	"image"
-	"image/color"
 	"log"
 	"math"
 	"time"
+
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"os"
+	"sync"
 )
 
 type Pix struct {
@@ -80,16 +85,14 @@ var (
 
 // router
 func main() {
-	pixelgl.Run(run)
-	/*
-	   router := gin.Default()
 
-	   router.GET("/mandelbrot", getMandelbrot)
-	   // router.GET("/albums/:id", getAlbumByID)
-	   // router.POST("/albums", postAlbums)
+	router := gin.Default()
 
-	   router.Run("localhost:8001")
-	*/
+	router.GET("/mandelbrot", getMandelbrot)
+	// router.GET("/albums/:id", getAlbumByID)
+	// router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8001")
 }
 
 // // getAlbums responds with the list of all albums as JSON.
@@ -99,7 +102,48 @@ func main() {
 //	}
 func getMandelbrot(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, mandelbrot)
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
 
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	var wg sync.WaitGroup
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		wg.Add(1)
+		go func(py int, y float64) {
+			defer wg.Done()
+			for px := 0; px < width; px++ {
+				x := float64(px)/width*(xmax-xmin) + xmin
+				z := complex(x, y)
+				// Image point (px, py) represents complex value z.
+				img.Set(px, py, makemandelbrot(z))
+			}
+		}(py, y)
+	}
+	wg.Wait()
+	f, err := os.Create("mandelbrot.png")
+	if err != nil {
+		panic(err)
+	}
+	png.Encode(f, img)
+
+	//png.Encode(os.Stdout, img) // NOTE: ignoring errors
+}
+
+func makemandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.Gray{255 - contrast*n}
+		}
+	}
+	return color.Black
 }
 
 // // postAlbums adds an album from JSON received in the request body.
